89ebe59646252e331126ba75001ba77c51b89821
mdazaz <85833855+mdazaz@users.noreply.github.com>
2022-01-06 00:43:39 +0530

    Update EcomResource.java

--- src/main/java/com/ecomerce/webapp/EcomResource.java
+++ src/main/java/com/ecomerce/webapp/EcomResource.java
@@ -14,7 +14,7 @@ public class EcomResource {
 	
 	@GetMapping("/")
 	public String index() {
-		return "<h1>Hello, Welcome ecommerce webservice application deployed using jenkins ! </h1>";
+		return '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"><div class="container"><h1>Hello, Welcome ecommerce webservice application deployed using jenkins ! </h1></div>';
 	}
 	
 	@GetMapping("/hello")
