e4fb960e1e5498f8e20c2374453fc2a7e8f4edd0
mdazaz <85833855+mdazaz@users.noreply.github.com>
2022-01-06 01:32:37 +0530

    Update EcomResource.java

--- src/main/java/com/ecomerce/webapp/EcomResource.java
+++ src/main/java/com/ecomerce/webapp/EcomResource.java
@@ -14,7 +14,7 @@ public class EcomResource {
 	
 	@GetMapping("/")
 	public String index() {
-		return "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'><main role='main' class='container'><div class='d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow'><img class='mr-3' src='https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-outline.svg' alt='' width='48' height='48'><div class='lh-100'><h6 class='mb-0 text-white lh-100'>eCommerce Web Application Deployed using Jenkins </h6> <small>Since 2021</small></div></div></main>";
+		return "<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'><body><div class='bg-1'><div class='container text-center'><h3>Who Am I?</h3><img src='http://www.awardaudio.com/image/obj600geo571pg41p11.jpg' class='img-circle' alt='Bird' width='350' height='350'><h3>Sample website deployed using Jenkins CI-CD by Md Azaz</h3></div></div></body>";
 	}
 	
 	@GetMapping("/hello")
